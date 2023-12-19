3350b1c420211ad9f2c5af0de7d8970de3de6443
Seheon Yu <developer@seheon.kr>
2023-12-19 19:00:35 +0900

    Add border and arrow to Select

--- src/components/atoms/select.tsx
+++ src/components/atoms/select.tsx
@@ -2,28 +2,53 @@ import React, { SelectHTMLAttributes } from 'react';
 
 import styled from '@emotion/styled';
 
+const arrowImage = ({ color }: { color: string }) =>
+  `url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23${color}%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')`;
+
 const StyledSelect = styled.select`
+  outline: none;
   appearance: none;
+  background-image: ${arrowImage({ color: '424242' })};
+  background-repeat: no-repeat;
+  background-position: right 0.7rem top 50%;
+  background-size: 0.65rem auto;
+
+  border: 1px solid #424242;
+  border-radius: 0.25rem;
+  width: 7.5rem;
+  padding: 0.5rem;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 400;
+
+  &:hover {
+    border-color: #ff908d;
+  }
+
+  &:focus {
+    border-color: #ff908d;
+    background-image: ${arrowImage({ color: 'ff908d' })};
+  }
 `;
 
-const StyledOptgroup = styled.optgroup``;
-const StyledOption = styled.option``;
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  placeholder?: string;
+}
 
-interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {}
-interface OptgroupProps extends SelectHTMLAttributes<HTMLOptGroupElement> {}
-interface OptionProps extends SelectHTMLAttributes<HTMLOptionElement> {}
-
-const Optgroup = ({ children, ...props }: OptgroupProps) => (
-  <StyledOptgroup {...props}>{children}</StyledOptgroup>
+export const Select = ({ placeholder, onChange, children, ...props }: SelectProps) => (
+  <StyledSelect
+    {...props}
+    onChange={(event) => {
+      event.target.blur();
+      onChange?.(event);
+    }}
+  >
+    <Select.Option disabled hidden selected>
+      {placeholder}
+    </Select.Option>
+    {children}
+  </StyledSelect>
 );
 
-const Option = ({ children, ...props }: OptionProps) => (
-  <StyledOption {...props}>{children}</StyledOption>
-);
-
-export const Select = ({ children, ...props }: SelectProps) => (
-  <StyledSelect {...props}>{children}</StyledSelect>
-);
-
-Select.Optgroup = Optgroup;
-Select.Option = Option;
+Select.Optgroup = styled.optgroup``;
+Select.Option = styled.option``;
