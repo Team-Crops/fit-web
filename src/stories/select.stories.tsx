2041634a8df1b8dd9e214aa76135c4616f84ea51
Seheon Yu <developer@seheon.kr>
2023-12-24 18:10:39 +0900

    Add value state in select.stories.tsx

--- src/stories/select.stories.tsx
+++ src/stories/select.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Meta, StoryObj } from '@storybook/react';
 import { Select } from 'src/components/atoms/select';
@@ -6,59 +6,80 @@ import { Select } from 'src/components/atoms/select';
 const meta = {
   title: 'Atom/Select',
   component: Select,
-  parameters: {
-    layout: 'centered',
-  },
   tags: ['atom', 'select'],
 } satisfies Meta<typeof Select>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const renderComponent = (args: Story['args']) => {
+function OptionsComponent(args: Story['args']) {
+  const [value, setValue] = useState<string>();
   return (
-    <Select {...args}>
-      <Select.Optgroup label="학력">
-        <Select.Option value="">고졸</Select.Option>
-        <Select.Option value="">대학교 1학년</Select.Option>
-        <Select.Option value="">대학교 2학년</Select.Option>
-        <Select.Option value="">대학교 3학년</Select.Option>
-        <Select.Option value="">대학교 4학년</Select.Option>
-        <Select.Option value="">대학원생</Select.Option>
-        <Select.Option value="">휴학생</Select.Option>
-      </Select.Optgroup>
-      <Select.Optgroup label="경력">
-        <Select.Option value="">인턴/계약직</Select.Option>
-        <Select.Option value="">신입(1년미만)</Select.Option>
-        <Select.Option value="">1년 ~ 3년</Select.Option>
-        <Select.Option value="">4년 ~ 7년</Select.Option>
-        <Select.Option value="">8년 ~ 10년</Select.Option>
-        <Select.Option value="">10년 이상</Select.Option>
-      </Select.Optgroup>
+    <Select
+      value={value}
+      helperText={`선택된 옵션의 value는 ${value} 입니다.`}
+      onChange={(e) => setValue(e.target.value)}
+      {...args}
+    >
+      <Select.Option value="0">없음</Select.Option>
+      <Select.Option value="1">1개</Select.Option>
+      <Select.Option value="2">2개</Select.Option>
+      <Select.Option value="3">3개</Select.Option>
+      <Select.Option value="4">4개</Select.Option>
+      <Select.Option value="5">5개</Select.Option>
+    </Select>
+  );
+}
+
+const OptionGroupsComponent = (args: Story['args']) => {
+  const [value, setValue] = useState<string>();
+  return (
+    <Select
+      value={value}
+      helperText={`선택된 옵션의 value는 ${value} 입니다.`}
+      onChange={(e) => setValue(e.target.value)}
+      {...args}
+    >
+      <Select.OptionGroup label="학력">
+        <Select.Option value="highschool">고졸</Select.Option>
+        <Select.Option value="freshman">대학교 1학년</Select.Option>
+        <Select.Option value="sophomore">대학교 2학년</Select.Option>
+        <Select.Option value="junior">대학교 3학년</Select.Option>
+        <Select.Option value="senior">대학교 4학년</Select.Option>
+        <Select.Option value="alumni">대학원생</Select.Option>
+        <Select.Option value="onleave">휴학생</Select.Option>
+      </Select.OptionGroup>
+      <Select.OptionGroup label="경력">
+        <Select.Option value="intern">인턴/계약직</Select.Option>
+        <Select.Option value="0">신입(1년미만)</Select.Option>
+        <Select.Option value="1">1년 ~ 3년</Select.Option>
+        <Select.Option value="4">4년 ~ 7년</Select.Option>
+        <Select.Option value="8">8년 ~ 10년</Select.Option>
+        <Select.Option value="10">10년 이상</Select.Option>
+      </Select.OptionGroup>
     </Select>
   );
 };
 
-export const MediumSize: Story = {
+export const WithOptions: Story = {
   args: {
     placeholder: '선택하세요.',
-    width: 'medium',
   },
-  render: renderComponent,
+  render: OptionsComponent,
 };
 
-export const LargeSize: Story = {
+export const WithOptionGroups: Story = {
   args: {
     placeholder: '선택하세요.',
-    width: 'large',
   },
-  render: renderComponent,
+  render: OptionGroupsComponent,
 };
 
 export const Error: Story = {
   args: {
     placeholder: '선택하세요.',
+    helperText: '필수 입력 항목입니다',
     error: true,
   },
-  render: renderComponent,
+  render: OptionsComponent,
 };
