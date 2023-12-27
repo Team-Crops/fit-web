fe1053dbd5811161b2269093068ce2542cfdb75a
jaewoongs <dbswodnd5428@gmail.com>
2023-12-27 22:26:29 +0900

    add Toggle Storybook

---
+++ src/stories/Toggle.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useEffect, useState } from 'react';
+import { Toggle } from 'src/components/atoms/Toggle';
+
+const meta = {
+  title: 'Atoms/Toggle',
+  component: Toggle,
+  tags: ['atom', 'toggle'],
+} satisfies Meta<typeof Toggle>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const ToggleTemplate = (args: Story['args']) => {
+  const [checked, setChecked] = useState(args.checked);
+
+  useEffect(() => {
+    setChecked(args.checked);
+  }, [args.checked]);
+
+  return (
+    <Toggle
+      {...args}
+      checked={checked}
+      onClick={() => {
+        setChecked((checked) => !checked);
+      }}
+    />
+  );
+};
+
+export const Primary: Story = {
+  args: {
+    checked: false,
+  },
+  render: ToggleTemplate,
+};
