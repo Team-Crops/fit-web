9d30f82b00d83e5b83de01dab83f91484b4c121c
Seheon Yu <developer@seheon.kr>
2023-12-29 01:16:38 +0900

    Add CheckBox component story

---
+++ src/stories/CheckBox.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+
+import { CheckBox } from 'src/components/atoms/CheckBox';
+
+const meta = {
+  title: 'Atoms/CheckBox',
+  component: CheckBox,
+  tags: ['atom', 'input'],
+} satisfies Meta<typeof CheckBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+function DefaultTemplate(args: Story['args']) {
+  const [isChecked, setChecked] = useState(args?.checked);
+  return (
+    <div style={{ display: 'flex' }}>
+      <CheckBox
+        id="template"
+        {...args}
+        checked={isChecked}
+        onChange={() => setChecked(!isChecked)}
+      />
+      <label htmlFor="template">{isChecked ? 'CHECKED' : 'UNCHECKED'}</label>
+    </div>
+  );
+}
+
+export const Default: Story = {
+  args: {
+    checked: false,
+    disabled: false,
+  },
+  render: DefaultTemplate,
+};
