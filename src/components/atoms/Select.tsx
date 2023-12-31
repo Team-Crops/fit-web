8a525a41ecc4ccabcce74f6cd91c7ad2ce2b1477
Seheon Yu <developer@seheon.kr>
2023-12-31 21:22:15 +0900

    Modify the code to conform to the convention
    - Use arrow functions
    - Change the order

--- src/components/atoms/Select.tsx
+++ src/components/atoms/Select.tsx
@@ -14,14 +14,15 @@ import type {
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
-const arrowSvg = (
-  color: string
-) => `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="9" viewBox="0 0 12 9" fill="none">
+function getArrowSvg(color: string) {
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="9" viewBox="0 0 12 9" fill="none">
   <path d="M6.4147 8.38433C6.21651 8.67856 5.78348 8.67856 5.5853 8.38433L0.462761 0.779331C0.239057 0.447218 0.47703 4.85377e-09 0.877459 3.51928e-08L11.1225 8.11426e-07C11.523 8.41765e-07 11.7609 0.447218 11.5372 0.779331L6.4147 8.38433Z" fill="${color}"/>
 </svg>`;
+}
 
-const arrowImage = ({ color }: { color: string }) =>
-  `url('data:image/svg+xml;charset=US-ASCII,${encodeURIComponent(arrowSvg(color))}')`;
+function getArrowImage({ color }: { color: string }) {
+  return `url('data:image/svg+xml;charset=US-ASCII,${encodeURIComponent(getArrowSvg(color))}')`;
+}
 
 const checkSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="11" viewBox="0 0 10 11" fill="none">
 <path d="M1 5L5 8.66667L9 1" stroke="#FF706C" stroke-width="2" stroke-linecap="round"/>
@@ -29,7 +30,7 @@ const checkSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="11"
 
 const checkImage = `url('data:image/svg+xml;charset=US-ASCII,${encodeURIComponent(checkSvg)}')`;
 
-const colorCSS = ({ error, value }: SelectProps) => {
+function getColorCSS({ error, value }: SelectProps) {
   if (error) {
     return css`
       color: #bdbdbd;
@@ -59,31 +60,31 @@ const colorCSS = ({ error, value }: SelectProps) => {
       }
     `;
   }
-};
+}
 
-const arrowCSS = ({ error }: SelectProps) => {
+function getArrowCSS({ error }: SelectProps) {
   if (error) {
     return css`
-      background-image: ${arrowImage({ color: '#bdbdbd' })};
+      background-image: ${getArrowImage({ color: '#bdbdbd' })};
       background-repeat: no-repeat;
       background-position: right 10px center;
 
       &:focus {
-        background-image: ${arrowImage({ color: '#ff0800' })};
+        background-image: ${getArrowImage({ color: '#ff0800' })};
       }
     `;
   } else {
     return css`
-      background-image: ${arrowImage({ color: '#9e9e9e' })};
+      background-image: ${getArrowImage({ color: '#9e9e9e' })};
       background-repeat: no-repeat;
       background-position: right 10px center;
 
       &:focus {
-        background-image: ${arrowImage({ color: '#ff908d' })};
+        background-image: ${getArrowImage({ color: '#ff908d' })};
       }
     `;
   }
-};
+}
 
 const SelectContainer = styled.div`
   display: flex;
@@ -106,8 +107,8 @@ const SelectButton = styled.button<SelectProps>`
   line-height: normal;
   letter-spacing: -0.6px;
 
-  ${colorCSS}
-  ${arrowCSS}
+  ${(props) => getColorCSS(props)}
+  ${(props) => getArrowCSS(props)}
 `;
 
 const OptionList = styled.ul`
@@ -136,56 +137,6 @@ const HelperText = styled.div<{ error?: boolean }>`
   letter-spacing: -0.4px;
 `;
 
-const SelectContext = createContext<{
-  value?: string | number | readonly string[];
-  onChange?: ChangeEventHandler<HTMLInputElement>;
-  setOpened?: Dispatch<SetStateAction<boolean>>;
-  setLabel?: Dispatch<SetStateAction<string | undefined>>;
-}>({});
-
-type SelectProps = InputHTMLAttributes<HTMLInputElement> & {
-  error?: boolean;
-  helperText?: string;
-};
-
-export function Select({
-  error,
-  helperText,
-  value,
-  onChange,
-  placeholder,
-  children,
-  ...props
-}: SelectProps) {
-  const [isOpened, setOpened] = useState(true);
-  const [label, setLabel] = useState<string | undefined>();
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
-        setOpened(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
-  });
-
-  return (
-    <SelectContext.Provider value={{ value, onChange, setOpened, setLabel }}>
-      <SelectContainer ref={containerRef}>
-        <input type="text" value={value} style={{ display: 'none' }} {...props} />
-        <SelectButton error={error} value={value} onClick={() => setOpened((prev) => !prev)}>
-          {label ?? placeholder}
-        </SelectButton>
-        {isOpened && <OptionList>{children}</OptionList>}
-        {helperText && <HelperText error={error}>{helperText}</HelperText>}
-      </SelectContainer>
-    </SelectContext.Provider>
-  );
-}
-
 const OptionGroupContainer = styled.div`
   margin-top: 8px;
 
@@ -208,21 +159,6 @@ const OptionGroupHr = styled.hr`
   margin: 0;
   border: 1px solid #eeeeee;
 `;
-
-type OptionGroupProps = {
-  label: string;
-  children?: ReactNode;
-};
-
-const OptionGroup = ({ label, children }: OptionGroupProps) => (
-  <OptionGroupContainer>
-    <OptionGroupLabel>{label}</OptionGroupLabel>
-    <OptionGroupHr />
-    {children}
-  </OptionGroupContainer>
-);
-
-Select.OptionGroup = OptionGroup;
 
 const OptionItem = styled.li<{ isSelected: boolean }>`
   cursor: pointer;
@@ -251,12 +187,77 @@ const OptionItem = styled.li<{ isSelected: boolean }>`
   }
 `;
 
+const SelectContext = createContext<{
+  value?: string | number | readonly string[];
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  setOpened?: Dispatch<SetStateAction<boolean>>;
+  setLabel?: Dispatch<SetStateAction<string | undefined>>;
+}>({});
+
+type SelectProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: boolean;
+  helperText?: string;
+};
+
+export const Select = ({
+  error,
+  helperText,
+  value,
+  onChange,
+  placeholder,
+  children,
+  ...props
+}: SelectProps) => {
+  const [isOpened, setOpened] = useState(true);
+  const [label, setLabel] = useState<string | undefined>();
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+        setOpened(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  });
+
+  return (
+    <SelectContext.Provider value={{ value, onChange, setOpened, setLabel }}>
+      <SelectContainer ref={containerRef}>
+        <input type="text" value={value} style={{ display: 'none' }} {...props} />
+        <SelectButton error={error} value={value} onClick={() => setOpened((prev) => !prev)}>
+          {label ?? placeholder}
+        </SelectButton>
+        {isOpened && <OptionList>{children}</OptionList>}
+        {helperText && <HelperText error={error}>{helperText}</HelperText>}
+      </SelectContainer>
+    </SelectContext.Provider>
+  );
+};
+
+type OptionGroupProps = {
+  label: string;
+  children?: ReactNode;
+};
+
+const OptionGroup = ({ label, children }: OptionGroupProps) => (
+  <OptionGroupContainer>
+    <OptionGroupLabel>{label}</OptionGroupLabel>
+    <OptionGroupHr />
+    {children}
+  </OptionGroupContainer>
+);
+
+Select.OptionGroup = OptionGroup;
+
 type OptionProps = {
   value: string;
   children?: ReactNode;
 };
 
-function Option({ value, children }: OptionProps) {
+const Option = ({ value, children }: OptionProps) => {
   const { value: currentValue, onChange, setOpened, setLabel } = useContext(SelectContext);
   return (
     <OptionItem
@@ -282,6 +283,6 @@ function Option({ value, children }: OptionProps) {
       {children}
     </OptionItem>
   );
-}
+};
 
 Select.Option = Option;
