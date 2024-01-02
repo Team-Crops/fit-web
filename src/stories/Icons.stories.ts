7cdcb78f964627ab12cbad45ec980f3bb68867bf
Seheon Yu <developer@seheon.kr>
2024-01-02 22:45:54 +0900

    Add icons stories

---
+++ src/stories/Icons.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Icons } from 'src/components/atoms/Icons';
+
+const meta = {
+  title: 'Atoms/Icons',
+  component: Icons,
+  tags: ['atom', 'icons'],
+} satisfies Meta<typeof Icons>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Icon: Story = {
+  args: {
+    icon: 'account',
+    width: 200,
+    height: 200,
+  },
+};
