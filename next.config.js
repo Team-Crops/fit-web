59d7d47e012ebfd51c282a0d36643ac7227e4d87
jaewoongs <dbswodnd5428@gmail.com>
2024-01-03 15:27:05 +0900

    test config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['src'],
+  },
   webpack: (config) => {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
