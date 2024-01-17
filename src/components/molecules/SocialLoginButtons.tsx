8d7dda599b68e1c935cbe954c40971b4d3d48322
Seheon Yu <developer@seheon.kr>
2024-01-17 15:03:11 +0900

    Add components for LoginEntrence

---
+++ src/components/molecules/SocialLoginButtons.tsx
@@ -0,0 +1,63 @@
+import { Button } from '#atoms/Button';
+import { Icons } from '#atoms/Icons';
+import { Txt } from '#atoms/Text';
+import styled from '@emotion/styled';
+
+const SocialButton = styled(Button)`
+  color: #616161;
+  width: 300px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    color: #212121;
+  }
+`;
+
+const KakaoButton = styled(SocialButton)`
+  background-color: #fbe80c;
+
+  &:hover {
+    background-color: #f7d30c;
+  }
+`;
+
+const GoogleButton = styled(SocialButton)`
+  background-color: #ffffff;
+
+  &:hover {
+    background-color: #f2f2f2;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const SocialLoginButtons = () => {
+  return (
+    <Column>
+      <KakaoButton variant="angular" height="60" color="primary">
+        <Icons icon="kakao" width={27} height={25} />
+        <div style={{ width: '18px' }} />
+        <Txt size="typo4" weight="regular">
+          카카오톡으로 로그인하기
+        </Txt>
+      </KakaoButton>
+      <GoogleButton variant="angular" height="60" color="primary">
+        <Icons icon="google" width={32} height={32} />
+        <div style={{ width: '18px' }} />
+        <Txt size="typo4" weight="regular">
+          구글 계정으로 로그인하기
+        </Txt>
+      </GoogleButton>
+    </Column>
+  );
+};
---
+++ src/components/organisms/LoginEntrance.tsx
@@ -0,0 +1,59 @@
+import { Icons } from '#atoms/Icons';
+import { Txt } from '#atoms/Text';
+import { SocialLoginButtons } from '#molecules/SocialLoginButtons';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  position: relative;
+  padding: 100px 250px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  border-radius: 15px;
+  background: linear-gradient(180deg, #f0f3ff 0%, #fff 100%);
+`;
+
+const CrossButton = styled.div`
+  position: absolute;
+  top: 30px;
+  right: 40px;
+  cursor: pointer;
+
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
+`;
+
+interface LoginEntranceProps {
+  onClose: () => void;
+}
+
+export const LoginEntrance = ({ onClose }: LoginEntranceProps) => (
+  <Container>
+    <CrossButton onClick={() => onClose()}>
+      <Icons icon="cross" width={20} height={20} color="#BDBDBD" />
+    </CrossButton>
+    <Txt size="typo1" weight="bold">
+      로그인
+    </Txt>
+    <div style={{ height: '30px' }} />
+    <Txt size="typo4" weight="regular">
+      F-it에 오신 것을 환영해요!
+    </Txt>
+    <Txt size="typo4" weight="regular">
+      간편 로그인으로 서비스를 이용해보세요 😉
+    </Txt>
+    <div style={{ height: '65px' }} />
+    <SocialLoginButtons />
+  </Container>
+);
