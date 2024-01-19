42b8350723e401fa8a8e447b9937f7cc4835ec2b
Seheon Yu <developer@seheon.kr>
2024-01-19 13:50:09 +0900

    Change SVG fill attribute to color

--- src/components/atoms/Icons.tsx
+++ src/components/atoms/Icons.tsx
@@ -1,48 +1,94 @@
 import React from 'react';
 
+import GoogleIcon from 'src/assets/icons/google.svg';
+import KakaoIcon from 'src/assets/icons/kakao.svg';
+
 import AccountIcon from 'src/assets/icons/account.svg';
 import ArrowForward from 'src/assets/icons/arrow-forward.svg';
 import ArrowRightIcon from 'src/assets/icons/arrow-right.svg';
 import AwayIcon from 'src/assets/icons/away.svg';
 import CheckIcon from 'src/assets/icons/check.svg';
 import ClickIcon from 'src/assets/icons/click.svg';
+import CrossIcon from 'src/assets/icons/cross.svg';
+import LogoIcon from 'src/assets/icons/logo.svg';
 import MegaphoneIcon from 'src/assets/icons/megaphone.svg';
 import RunIcon from 'src/assets/icons/run.svg';
 
+type IconName =
+  | 'account'
+  | 'arrowForward'
+  | 'arrowRight'
+  | 'away'
+  | 'check'
+  | 'click'
+  | 'cross'
+  | 'google'
+  | 'kakao'
+  | 'logo'
+  | 'megaphone'
+  | 'run';
+
+interface Icon {
+  SVGR: any;
+  color?: string;
+}
+
+const icons: Record<IconName, Icon> = {
+  account: {
+    SVGR: AccountIcon,
+    color: '#212121',
+  },
+  arrowForward: {
+    SVGR: ArrowForward,
+    color: '#919191',
+  },
+  arrowRight: {
+    SVGR: ArrowRightIcon,
+    color: '#212121',
+  },
+  away: {
+    SVGR: AwayIcon,
+    color: '#212121',
+  },
+  check: {
+    SVGR: CheckIcon,
+    color: '#9E9E9E',
+  },
+  click: {
+    SVGR: ClickIcon,
+    color: '#212121',
+  },
+  cross: {
+    SVGR: CrossIcon,
+    color: '#212121',
+  },
+  google: {
+    SVGR: GoogleIcon,
+  },
+  kakao: {
+    SVGR: KakaoIcon,
+  },
+  logo: {
+    SVGR: LogoIcon,
+    color: '#FF706C',
+  },
+  megaphone: {
+    SVGR: MegaphoneIcon,
+  },
+  run: {
+    SVGR: RunIcon,
+    color: '#212121',
+  },
+};
+
 interface IconsProps {
-  icon:
-    | 'account'
-    | 'arrow-forward'
-    | 'arrow-right'
-    | 'away'
-    | 'check'
-    | 'click'
-    | 'megaphone'
-    | 'run';
+  icon: IconName;
   color?: string;
   width?: number;
   height?: number;
 }
 
 export const Icons = ({ icon, color, width, height }: IconsProps) => {
-  switch (icon) {
-    case 'account':
-      return <AccountIcon width={width} height={height} fill={color ?? '#212121'} />;
-    case 'arrow-forward':
-      return <ArrowForward width={width} height={height} fill={color ?? '#919191'} />;
-    case 'arrow-right':
-      return <ArrowRightIcon width={width} height={height} stroke={color ?? '#212121'} />;
-    case 'away':
-      return <AwayIcon width={width} height={height} fill={color ?? '#212121'} />;
-    case 'check':
-      return <CheckIcon width={width} height={height} stroke={color ?? '#9E9E9E'} />;
-    case 'click':
-      return <ClickIcon width={width} height={height} fill={color ?? '#212121'} />;
-    case 'megaphone':
-      return <MegaphoneIcon width={width} height={height} fill={color ?? '#212121'} />;
-    case 'run':
-      return <RunIcon width={width} height={height} fill={color ?? '#212121'} />;
-    default:
-      throw new Error(`Icon ${icon} is not supported!`);
-  }
+  const { SVGR, color: defaultColor } = icons[icon];
+  return <SVGR width={width} height={height} color={color || defaultColor} />;
 };
