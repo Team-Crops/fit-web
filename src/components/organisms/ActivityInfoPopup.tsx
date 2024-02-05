b9f6fa1573520e30c982a36360b1f34e17ced87b
Seheon Yu <developer@seheon.kr>
2024-02-05 18:57:49 +0900

    Add RegionSelect component and update ActivityInfoPopup

--- src/components/organisms/ActivityInfoPopup.tsx
+++ src/components/organisms/ActivityInfoPopup.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styled from '@emotion/styled';
 
+import { AuthStep, updateAuth } from '#/redux/features/auth/slice';
+import { useUpdateMeMutation } from '#/redux/features/user/api';
+import { useAppDispatch } from '#/redux/hooks';
 import { Icons } from '#atoms/Icons';
 import { Select } from '#atoms/Select';
 import { Txt } from '#atoms/Text';
+import { RegionSelect } from '#molecules/RegionSelect';
 import { SignupProgressBar } from '#molecules/SignupProgressBar';
 
 const Container = styled.div`
@@ -52,11 +56,30 @@ const StyledSelect = styled(Select)`
 const Spacer = styled.div``;
 
 export const ActivityInfoPopup = () => {
+  const dispatch = useAppDispatch();
+  const [updateMe, { isLoading, isSuccess, isError }] = useUpdateMeMutation();
+
   const [projectCount, setProjectCount] = useState<number>();
+  const [regionId, setRegionId] = useState<number>();
+  const [activityHours, setActivityHours] = useState<number>();
+
+  useEffect(() => {}, [isLoading, isSuccess, isError]);
 
   return (
     <Container>
-      <SignupProgressBar currentStep={3} totalStep={4} progressName="활동정보" />
+      <SignupProgressBar
+        currentStep={3}
+        totalStep={4}
+        progressName="활동정보"
+        onBackwardClick={() => {
+          dispatch(updateAuth({ step: AuthStep.ActivityInfo - 1 }));
+        }}
+        onForwardClick={
+          [projectCount, regionId, activityHours].every((v) => v !== undefined)
+            ? () => updateMe({ projectCount, regionId, activityHours })
+            : undefined
+        }
+      />
       <TitleContainer>
         <Txt size="typo1" weight="bold">
           나의 프로젝트 경험과 활동 정보를 알려주세요
@@ -71,26 +94,39 @@ export const ActivityInfoPopup = () => {
             프로젝트 경험 수
           </Txt>
           <StyledSelect
-            value={projectCount}
-            onChange={(e) => setProjectCount(e.target.valueAsNumber)}
+            value={projectCount ? projectCount?.toString() : ''}
+            onChange={(e) => setProjectCount(parseInt(e.target.value, 10))}
           >
-            <Select.Option value={1}>1번</Select.Option>
-            <Select.Option value={2}>2번</Select.Option>
-            <Select.Option value={3}>3번</Select.Option>
-            <Select.Option value={4}>4번</Select.Option>
+            {[...Array(4)].map((_, i) => (
+              <Select.Option key={i} value={i}>
+                {i === 0 ? '없음' : i === 3 ? '3회 이상' : `${i}회`}
+              </Select.Option>
+            ))}
           </StyledSelect>
         </InputContainer>
         <InputContainer>
           <Txt size="typo5" weight="medium">
             주 활동 지역
           </Txt>
-          <StyledSelect />
+          <RegionSelect
+            value={regionId}
+            onChange={(e) => setRegionId(parseInt(e.target.value, 10))}
+          />
         </InputContainer>
         <InputContainer>
           <Txt size="typo5" weight="medium">
             활동 가능 시간
           </Txt>
-          <StyledSelect />
+          <StyledSelect
+            value={activityHours}
+            onChange={(e) => setActivityHours(parseInt(e.target.value, 10))}
+          >
+            {[...Array(4)].map((_, i) => (
+              <Select.Option key={i} value={3 * 2 ** i}>
+                {3 * 2 ** i}시간
+              </Select.Option>
+            ))}
+          </StyledSelect>
         </InputContainer>
       </FormContainer>
       <Spacer />
