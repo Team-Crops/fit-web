a759f219eb0ec6b7bc2b014ec148a3dba39f7d4c
Seheon Yu <developer@seheon.kr>
2024-02-15 18:10:10 +0900

    Add, pulse animation

--- src/components/organisms/SkillInfoPopup.tsx
+++ src/components/organisms/SkillInfoPopup.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { Position } from '#/entities/position';
@@ -49,14 +50,17 @@ const PositionTabContainer = styled.div`
 
 const PositionTabs = styled.div`
   display: flex;
+  gap: 10px;
+
+  padding: 10px;
 `;
 
-const PositionTab = styled(Button)<{ selected: boolean }>`
-  margin: 10px;
-  padding: 10px 0;
+const PositionTab = styled(Button)<{ selected?: boolean; loading?: boolean }>`
   flex-grow: 1;
 
-  background-color: transparent;
+  animation: ${({ loading }) =>
+    loading ? 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite' : 'none'};
+  background-color: ${({ loading }) => (loading ? '#f5f5f5' : 'transparent')};
   color: ${({ selected }) => (selected ? '#ff706c' : '#9e9e9e')};
 
   &:hover {
@@ -75,72 +79,26 @@ const SkillsContainer = styled.div`
 
   padding: 36px;
   height: 220px;
+
+  overflow-y: scroll;
 `;
 
-const SkillButton = styled(Button)`
+const SkillButton = styled(Button)<{ loading?: boolean }>`
   padding: 10px 40px;
+
+  ${({ loading }) =>
+    loading &&
+    css`
+      animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+      background-color: #f5f5f5;
+      border: none;
+      width: 150px;
+    `}
 `;
 
 export const SkillInfoPopup = () => {
-  // const { data: positions, isLoading: isLoadingPositions } = useGetPositionsQuery();
-  const positions: { positionList: Position[] } = {
-    positionList: [
-      {
-        id: 1,
-        displayName: '기획자',
-        skillList: [],
-      },
-      {
-        id: 2,
-        displayName: '디자이너',
-        skillList: [],
-      },
-      {
-        id: 3,
-        displayName: '서버 개발자',
-        skillList: [],
-      },
-      {
-        id: 4,
-        displayName: '웹 프론트 개발자',
-        skillList: [],
-      },
-      {
-        id: 5,
-        displayName: '앱 개발자',
-        skillList: [],
-      },
-    ],
-  };
-  // const { data: skills, isLoading: isLoadingSkills } = useGetSkillsQuery();
-  const skills: { skillList: Skill[] } = {
-    skillList: [
-      {
-        id: 1,
-        displayName: 'Figma',
-      },
-      {
-        id: 2,
-        displayName: 'Photoshop',
-      },
-      {
-        id: 3,
-        displayName: 'Illustrator',
-      },
-      {
-        id: 4,
-        displayName: 'Zeplin',
-      },
-      {
-        id: 5,
-        displayName: 'Photoshop',
-      },
-      {
-        id: 6,
-        displayName: 'Adobe XD',
-      },
-    ],
-  };
+  const { data: positions, isLoading: isLoadingPositions } = useGetPositionsQuery();
+  const { data: skills, isLoading: isLoadingSkills } = useGetSkillsQuery();
   const [selectedPosition, setSelectedPosition] = useState<number>();
   const [selectedSkills, setSelectedSkills] = useState<number[]>([]);
 
@@ -166,40 +124,65 @@ export const SkillInfoPopup = () => {
       <ContentsContainer>
         <PositionTabContainer>
           <PositionTabs>
-            {positions?.positionList.map((position) => (
-              <PositionTab
-                key={position.id}
-                variant="angular"
-                height="70"
-                color="primary"
-                selected={selectedPosition === position.id}
-                onClick={() => setSelectedPosition(position.id)}
-              >
-                <Txt size="typo5" weight={position.id === selectedPosition ? 'bold' : 'medium'}>
-                  {position.displayName}
-                </Txt>
-              </PositionTab>
-            ))}
+            {isLoadingPositions
+              ? Array.from({ length: 5 }).map((_, index) => (
+                  <PositionTab
+                    key={index}
+                    variant="angular"
+                    height="70"
+                    color="primary"
+                    disabled
+                    loading
+                  />
+                ))
+              : positions?.positionList.map((position) => (
+                  <PositionTab
+                    key={position.id}
+                    variant="angular"
+                    height="70"
+                    color="primary"
+                    selected={selectedPosition === position.id}
+                    onClick={() => setSelectedPosition(position.id)}
+                  >
+                    <Txt size="typo5" weight={position.id === selectedPosition ? 'bold' : 'medium'}>
+                      {position.displayName}
+                    </Txt>
+                  </PositionTab>
+                ))}
           </PositionTabs>
           <Divider style={{ margin: '0 10px', width: 'calc(100% - 20px)' }} />
           <SkillsContainer>
-            {skills?.skillList.map((skill) => (
-              <SkillButton
-                key={skill.id}
-                onClick={() => skillClickHandler(skill.id)}
-                variant="outlined"
-                height="50"
-                color={selectedSkills.includes(skill.id) ? 'primary' : 'secondary'}
-              >
-                {skill.displayName}
-              </SkillButton>
-            ))}
+            {isLoadingSkills
+              ? Array.from({ length: 5 }).map((_, index) => (
+                  <SkillButton
+                    key={index}
+                    variant="outlined"
+                    height="50"
+                    color="secondary"
+                    disabled
+                    loading
+                  />
+                ))
+              : skills?.skillList.map((skill) => (
+                  <SkillButton
+                    key={skill.id}
+                    onClick={() => skillClickHandler(skill.id)}
+                    variant="outlined"
+                    height="50"
+                    color={selectedSkills.includes(skill.id) ? 'primary' : 'secondary'}
+                  >
+                    <Txt size="typo5" weight="medium">
+                      {skill.displayName}
+                    </Txt>
+                  </SkillButton>
+                ))}
           </SkillsContainer>
         </PositionTabContainer>
         <Txt size="typo5" weight="regular" style={{ color: '#9e9e9e' }}>
           *다른 포지션의 툴도 선택할 수 있어요.
         </Txt>
       </ContentsContainer>
+      <div />
     </Container>
   );
 };
