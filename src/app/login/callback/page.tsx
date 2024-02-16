978884a0cffa7103a922b611c95a76836fcc4e77
Seheon Yu <developer@seheon.kr>
2024-02-16 17:20:39 +0900

    Remove unused login callback page

--- src/app/login/callback/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { redirect, useSearchParams } from 'next/navigation';
-
-export default function LoginCallback() {
-  const searchParams = useSearchParams();
-  const code = searchParams.get('code');
-  redirect(`/login/kakao/callback?code=${code}`);
-}
