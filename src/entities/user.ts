cb7b7ce7d8d5bb3d53b15f7371f46a31d807c452
Seheon Yu <developer@seheon.kr>
2024-02-17 13:12:29 +0900

    Refactor, User interface in user.ts

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,23 +1,24 @@
-import { Contact } from './contact';
+import { Link } from './link';
 
 export interface User {
   id: number;
-  profileImageUrl: string | null;
-  username: string | null;
-  nickname: string | null;
-  phoneNumber: string | null;
-  email: string | null;
-  backgroundStatus: string | null;
-  backgroundText: string | null;
-  portfolioUrl: string | null;
-  projectCount: number | null;
-  activityHours: number | null;
-  introduction: string | null;
-  contacts: Contact[];
-  positionId: number | null;
-  regionId: number | null;
-  skillIds: number[];
-  status: string | null;
-  isPublicPhoneNumber: boolean;
-  isPublicProfile: boolean;
+
+  projectCount?: number;
+  activityHours?: number;
+  introduce?: string;
+  portfolioUrl?: string;
+  backgroundText?: string;
+  isOpenPhoneNum?: boolean;
+  linkList?: Link[];
+  isOpenProfile?: boolean;
+  phoneNumber?: string;
+  positionId?: number;
+  regionId?: number;
+  backgroundStatus?: string;
+  nickname?: string;
+  skillIdList?: number[];
+  profileImageUrl?: string;
+  email?: string;
+  status?: string;
+  username?: string;
 }
