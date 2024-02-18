abceefcecc8cb71fe5fd286514f56267343b955a
Seheon Yu <developer@seheon.kr>
2024-02-18 11:07:35 +0900

    Add policy agreement endpoints and types

--- src/redux/features/user/api.ts
+++ src/redux/features/user/api.ts
@@ -1,8 +1,19 @@
+import { PolicyAgreement } from '#/entities/policyAgreement';
 import { User } from '#/entities/user';
 import { api } from '#/redux/api';
 
 export interface UserUpdateRequest extends Partial<Omit<User, 'id' | 'status'>> {}
 export interface UserUpdateResponse extends Partial<Omit<User, 'id'>> {}
+
+export interface PolicyAgreementResponse {
+  policyAgreementList: PolicyAgreement[];
+}
+export interface PolicyAgreementUpdateRequest {
+  policyAgreementList: PolicyAgreement[];
+}
+export interface PolicyAgreementUpdateResponse {
+  policyAgreementList: PolicyAgreement[];
+}
 
 const userApi = api.injectEndpoints({
   endpoints: (build) => ({
@@ -19,8 +30,34 @@ const userApi = api.injectEndpoints({
         body: user,
       }),
     }),
+    myAgreements: build.query<PolicyAgreementResponse, void>({
+      query: () => ({
+        url: `/v1/user/policy-agreement`,
+        method: 'GET',
+      }),
+    }),
+    updateMyAgreements: build.mutation<PolicyAgreementUpdateResponse, PolicyAgreementUpdateRequest>(
+      {
+        query: (agreements) => ({
+          url: `/v1/user/policy-agreement`,
+          method: 'PUT',
+          body: {
+            policyAgreementList: agreements.policyAgreementList.map((agreement) => ({
+              ...agreement,
+              updatedAt: new Date().toISOString(),
+            })),
+          },
+        }),
+      }
+    ),
   }),
 });
 
-export const { useMeQuery, useLazyMeQuery, useUpdateMeMutation } = userApi;
+export const {
+  useMeQuery,
+  useLazyMeQuery,
+  useUpdateMeMutation,
+  useMyAgreementsQuery,
+  useUpdateMyAgreementsMutation,
+} = userApi;
 export default userApi;
