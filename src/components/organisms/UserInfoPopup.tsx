9eed86072d946adcbb2d48c27f20ac751e3c4c82
Seheon Yu <developer@seheon.kr>
2024-02-18 10:11:03 +0900

    Add, onSubmitEventHandler

--- src/components/organisms/UserInfoPopup.tsx
+++ src/components/organisms/UserInfoPopup.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styled from '@emotion/styled';
 
-import { usePresignedProfileImageUploadUrlMutation } from '#/redux/features/file/api';
+import { useLazyGetUploadSignedUrlQuery } from '#/redux/features/file/api';
 import { useUpdateMeMutation } from '#/redux/features/user/api';
-import { useAppDispatch } from '#/redux/hooks';
 import { Button } from '#atoms/Button';
 import { Icons } from '#atoms/Icons';
 import { Txt } from '#atoms/Text';
@@ -44,6 +43,8 @@ const ProfileImage = styled.div`
   &:hover {
     box-shadow: 0px 0px 164px rgba(0, 0, 0, 0.1);
   }
+
+  overflow: hidden;
 `;
 
 const NicknameContainer = styled.div`
@@ -64,53 +65,96 @@ const StyledInput = styled.input`
   }
 `;
 
-const HelperText = styled(Txt)`
-  color: #9e9e9e;
-`;
-
 export const UserInfoPopup = () => {
-  const [updateMe, { data, isLoading }] = useUpdateMeMutation();
-  const [getUploadUrl, { data: uploadInfo }] = usePresignedProfileImageUploadUrlMutation();
+  const [updateMe] = useUpdateMeMutation();
+  const [getSignedUrl, { data: signedUrl }] = useLazyGetUploadSignedUrlQuery();
 
-  const dispatch = useAppDispatch();
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const [nickname, setNickname] = useState('');
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+  const [enableContinue, setEnableContinue] = useState(false);
 
-  const profileImageInputRef = useRef<HTMLInputElement>(null);
-  const nicknameInputRef = useRef<HTMLInputElement>(null);
-
-  const onProfileImageClick = () => {
-    profileImageInputRef.current?.click();
+  const handleImageChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const image = e.target.files?.item(0);
+    if (image) {
+      setPreviewImage(URL.createObjectURL(image));
+    } else {
+      setPreviewImage(null);
+    }
   };
-  const onProfileImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.item(0);
-    console.log(file);
-  };
-  const onSubmitClick = () => {};
+
+  useEffect(() => {
+    setEnableContinue(nickname.length > 0);
+  }, [imageInputRef, nickname]);
+
+  useEffect(() => {
+    if (signedUrl) {
+      const image = imageInputRef.current?.files?.item(0);
+      if (!image) {
+        return;
+      }
+      const formData = new FormData();
+      formData.append('file', image);
+      fetch(signedUrl.preSignedUrl, {
+        method: 'PUT',
+        body: image,
+      }).then(() => {
+        updateMe({ nickname, profileImageUrl: signedUrl.fileKey });
+      });
+    }
+  });
 
   return (
     <Container>
       <Txt size="typo1" weight="bold">
         프로필 사진과 닉네임을 설정해주세요!
       </Txt>
-      <ProfileImage onClick={onProfileImageClick}>
-        <Icons icon="camera" width={48} height={48} />
-        <Txt size="typo4" weight="medium">
-          이미지 추가
-        </Txt>
+      <ProfileImage onClick={() => imageInputRef.current?.click()}>
+        {previewImage ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={previewImage} alt="Profile Image" width="164" height="164" />
+        ) : (
+          <>
+            <Icons icon="camera" width={48} height={48} />
+            <Txt size="typo4" weight="medium">
+              이미지 추가
+            </Txt>
+          </>
+        )}
         <input
           type="file"
           accept="image/*"
-          onChange={onProfileImageChange}
+          onChange={handleImageChange}
           hidden
-          ref={profileImageInputRef}
+          ref={imageInputRef}
         />
       </ProfileImage>
       <NicknameContainer>
-        <StyledInput placeholder="닉네임" ref={nicknameInputRef} />
-        <HelperText size="typo5" weight="regular">
+        <StyledInput
+          placeholder="닉네임"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+        <Txt style={{ color: '#9e9e9e' }} size="typo5" weight="regular">
           필수항목입니다
-        </HelperText>
+        </Txt>
       </NicknameContainer>
-      <Button color="primary" height="70" variant="round" onClick={onSubmitClick}>
+      <Button
+        color="primary"
+        height="70"
+        variant="round"
+        disabled={!enableContinue}
+        onClick={() => {
+          const image = imageInputRef.current?.files?.item(0);
+          if (!image) {
+            return;
+          }
+          getSignedUrl({
+            fileDomain: 'PROFILE_IMAGE',
+            fileName: image.name,
+          });
+        }}
+      >
         시작하기
       </Button>
     </Container>
