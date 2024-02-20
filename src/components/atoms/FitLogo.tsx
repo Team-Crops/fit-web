7a776ade181cf8e87dda5422d1ec9dbf5f6a87f1
Seheon Yu <developer@seheon.kr>
2024-02-20 18:09:14 +0900

    Refactor FitLogo component to use colorMap

--- src/components/atoms/FitLogo.tsx
+++ src/components/atoms/FitLogo.tsx
@@ -1,17 +1,15 @@
-import styled from '@emotion/styled';
 import Logo from 'src/assets/logo.svg';
-
-const StyledLogo = styled(Logo)`
-  path {
-    ${({ color }) => color === 'gray' && `fill: #757575;`}
-  }
-`;
 
 interface LogoProps {
   width: number;
   height: number;
   color?: 'primary' | 'gray';
 }
-export const FitLogo = ({ width, height, color }: LogoProps) => {
-  return <StyledLogo width={width} height={height} color={color} />;
+export const FitLogo = ({ width, height, color = 'primary' }: LogoProps) => {
+  const colorMap = {
+    primary: '#FF706C',
+    gray: '#757575',
+  };
+
+  return <Logo width={width} height={height} color={colorMap[color]} />;
 };
