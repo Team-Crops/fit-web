dca3221f6d33de98198a190cf44e4b560440e415
Seheon Yu <developer@seheon.kr>
2024-03-01 15:29:52 +0900

    Add isStudent and isWorker functions to User entity

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -41,3 +41,30 @@ export interface User {
   status?: string;
   username?: string;
 }
+
+const studentStatuses: UserBackgroundStatusStudent[] = [
+  'HIGH_SCHOOL_GRADUATE',
+  'UNIVERSITY_FRESHMAN',
+  'UNIVERSITY_SOPHOMORE',
+  'UNIVERSITY_JUNIOR',
+  'UNIVERSITY_SENIOR',
+  'GRADUATE_STUDENT',
+  'LEAVE_OF_ABSENCE_STUDENT',
+];
+
+const workerStatuses: UserBackgroundStatusWorker[] = [
+  'WORKER_CONTRACT_WORKER',
+  'WORKER_LESS_THAN_ONE_YEAR',
+  'WORKER_ONE_TO_THREE_YEARS',
+  'WORKER_FOUR_TO_SEVEN_YEARS',
+  'WORKER_EIGHT_TO_TEN_YEARS',
+  'WORKER_OVER_TEN_YEARS',
+];
+
+export function isStudent(status: UserBackgroundStatus): status is UserBackgroundStatusStudent {
+  return studentStatuses.includes(status as UserBackgroundStatusStudent);
+}
+
+export function isWorker(status: UserBackgroundStatus): status is UserBackgroundStatusWorker {
+  return workerStatuses.includes(status as UserBackgroundStatusWorker);
+}
