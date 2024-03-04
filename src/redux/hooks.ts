a5090208fec111fabeefa484002fdcecf5f36d32
Seheon Yu <developer@seheon.kr>
2024-03-04 19:55:31 +0900

    Remove useMe hook from redux/hooks.ts

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -6,7 +6,3 @@ import type { AppDispatch, AppStore, RootState } from 'src/redux/store';
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppStore: () => AppStore = useStore;
-
-export const useMe = () => {
-  return useAppSelector((state) => state.auth.user);
-};
