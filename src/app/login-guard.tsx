d99fa18013b603ca53ed92822bea0e71e5d83466
Seheon Yu <developer@seheon.kr>
2024-03-10 20:16:15 +0900

    Update auth and user features

--- src/app/login-guard.tsx
+++ src/app/login-guard.tsx
@@ -2,8 +2,13 @@
 
 import { useEffect } from 'react';
 
+import _ from 'lodash';
+
 import { Login } from '#/components/templates/Login';
-import { updateAuth } from '#/redux/features/auth/slice';
+import { type PolicyType, policies } from '#/entities/policy';
+import { AuthStep, deleteAuth, updateAuth } from '#/redux/features/auth/slice';
+import { useLazyMeQuery, useLazyMyAgreementsQuery } from '#/redux/features/user/api';
+import { setMe } from '#/redux/features/user/slice';
 import { useAppDispatch, useAppSelector } from '#/redux/hooks';
 
 interface LoginGuardProps {
@@ -13,14 +18,55 @@ interface LoginGuardProps {
 export function LoginGuard({ children }: LoginGuardProps) {
   const dispatch = useAppDispatch();
 
-  const signupStatus = useAppSelector((state) => state.user.me?.status);
   const showLoginPopup = useAppSelector((state) => state.auth.showLoginPopup);
 
+  const [queryMe, { data: me, isSuccess: queryMeSuccess, isError: queryMeError }] =
+    useLazyMeQuery();
+  const [queryAgreements, { data: agreements }] = useLazyMyAgreementsQuery();
+
   useEffect(() => {
-    if (signupStatus === 'INCOMPLETE') {
-      dispatch(updateAuth({ showLoginPopup: true }));
+    const accessToken = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+    dispatch(updateAuth({ accessToken, refreshToken }));
+    queryMe();
+    queryAgreements();
+  }, [dispatch, queryAgreements, queryMe]);
+
+  useEffect(() => {
+    if (queryMeSuccess && me && agreements) {
+      let step: AuthStep;
+
+      if (
+        _.some(
+          policies,
+          (policy, policyName) => policy.required && !agreements[policyName as PolicyType]
+        )
+      ) {
+        step = AuthStep.Policies;
+      } else if (!me.nickname) {
+        step = AuthStep.UserInfo;
+      } else if (!me.positionId) {
+        step = AuthStep.PositionInfo;
+      } else if (!me.username || !me.email || !me.backgroundStatus || !me.backgroundText) {
+        step = AuthStep.PersonalInfo;
+      } else if (!me.projectCount || !me.regionId || !me.activityHour) {
+        step = AuthStep.ActivityInfo;
+      } else if (!me.skillIdList) {
+        step = AuthStep.SkillInfo;
+      } else {
+        step = AuthStep.Complete;
+      }
+
+      dispatch(setMe(me));
+      dispatch(updateAuth({ step, showLoginPopup: me.status === 'INCOMPLETE' }));
     }
-  }, [dispatch, signupStatus]);
+  }, [agreements, dispatch, me, queryMeSuccess]);
+
+  useEffect(() => {
+    if (queryMeError) {
+      dispatch(deleteAuth());
+    }
+  }, [dispatch, queryMeError]);
 
   return (
     <>
