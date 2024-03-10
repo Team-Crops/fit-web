834dec6d23ca953062aaedb999921365597f1e64
Seheon Yu <developer@seheon.kr>
2024-03-10 17:45:25 +0900

    Add matching page component

---
+++ src/app/matching/page.tsx
@@ -0,0 +1,11 @@
+import { Footer } from '#templates/Footer';
+import { Header } from '#templates/Header';
+
+export default function Matching() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
