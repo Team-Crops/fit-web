8592641b32fde073fa8fa33cf95e1a3dc1a72805
Seheon Yu <developer@seheon.kr>
2024-03-11 10:31:23 +0900

    Add progress bar tip

--- src/components/molecules/matching/MatchingProgressBar.tsx
+++ src/components/molecules/matching/MatchingProgressBar.tsx
@@ -1,11 +1,34 @@
 import styled from '@emotion/styled';
 
 import { ProgressBar } from '#/components/atoms/ProgressBar';
+import { Txt } from '#/components/atoms/Text';
 
 const StyledProgressBar = styled(ProgressBar)`
   height: 8px;
 `;
 
+const TooltipContainer = styled.div`
+  position: relative;
+  width: fit-content;
+  left: 33%;
+  transform: translateX(-50%);
+
+  border-radius: 100px;
+  padding: 9px 16px;
+  background-color: #ffa7a5;
+  color: #ffffff;
+`;
+
 export function MatchingProgressBar() {
-  return <StyledProgressBar current={1} total={3} />;
+  return (
+    <>
+      <StyledProgressBar current={1} total={3} />
+      <div style={{ height: '10px' }} />
+      <TooltipContainer>
+        <Txt size="typo6" weight="bold">
+          1/3 포지션 확인
+        </Txt>
+      </TooltipContainer>
+    </>
+  );
 }
