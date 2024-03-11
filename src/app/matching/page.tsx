6630c2440cc5e639b47dde551c5690320f122f24
Seheon Yu <developer@seheon.kr>
2024-03-11 10:25:53 +0900

    Fix to align center

--- src/app/matching/page.tsx
+++ src/app/matching/page.tsx
@@ -1,15 +1,25 @@
 'use client';
 
+import styled from '@emotion/styled';
+
 import { Footer } from '#templates/Footer';
 import { Header } from '#templates/Header';
 import { MatchingTemplate } from '#templates/MatchingTemplate';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+`;
+
 export default function Matching() {
   return (
-    <>
+    <Container>
       <Header />
       <MatchingTemplate />
       <Footer />
-    </>
+    </Container>
   );
 }
