f03a61a33b1014731bc684b85c80349e05f07190
Seheon Yu <developer@seheon.kr>
2024-03-11 16:57:14 +0900

    Add buttons to MatchingTemplate

--- src/components/templates/MatchingTemplate.tsx
+++ src/components/templates/MatchingTemplate.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+import { Button } from '#atoms/Button';
 import { MatchingProgressBar } from '#molecules/matching/MatchingProgressBar';
 import { MatchingTitle } from '#molecules/matching/MatchingTitle';
 import { MatchingProfile } from '#organisms/matching/MatchingProfile';
@@ -7,9 +8,17 @@ import { MatchingProfile } from '#organisms/matching/MatchingProfile';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
 
   width: 100%;
   max-width: 1200px;
+  min-height: calc(100vh - 100px);
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
 `;
 
 export function MatchingTemplate() {
@@ -19,6 +28,14 @@ export function MatchingTemplate() {
       <div style={{ height: '35px' }} />
       <MatchingProgressBar />
       <MatchingProfile />
+      <ButtonsContainer>
+        <Button variant="round" height="70" color="secondary">
+          수정하기
+        </Button>
+        <Button variant="round" height="70" color="primary">
+          확인
+        </Button>
+      </ButtonsContainer>
     </Container>
   );
 }
