ca2024ec03be7a85f049302668c841a4701fd9c4
Seheon Yu <seheon@seoultech.ac.kr>
2024-03-19 00:21:07 +0900

    Add tailPosition to TalkBubble component

--- src/components/molecules/TalkBubble.tsx
+++ src/components/molecules/TalkBubble.tsx
@@ -2,18 +2,27 @@ import styled from '@emotion/styled';
 
 import { Txt } from '#atoms/Text';
 
-const Container = styled.div``;
-
-const TextBubble = styled(Txt)`
+const TextBubble = styled(Txt)<{ tailPosition: TalkBubbleProps['tailPosition'] }>`
   background: #fff;
   padding: 20px 35px 20px 25px;
-  border-radius: 0 40px 40px 40px;
+  border-radius: ${({ tailPosition }) =>
+    tailPosition === 'left' ? '0 40px 40px 40px' : '40px 0 40px 40px'};
   filter: drop-shadow(0px 0px 40px rgba(0, 0, 0, 0.08));
   line-height: 2;
 `;
 
-interface TalkBubbleProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface TalkBubbleProps extends React.HTMLAttributes<HTMLDivElement> {
+  tailPosition?: 'left' | 'right';
+}
 
-export function TalkBubble({ ...props }: TalkBubbleProps) {
-  return <TextBubble size="typo4" weight="regular" color="#424242" {...props} />;
+export function TalkBubble({ tailPosition = 'left', ...props }: TalkBubbleProps) {
+  return (
+    <TextBubble
+      tailPosition={tailPosition}
+      size="typo4"
+      weight="regular"
+      color="#424242"
+      {...props}
+    />
+  );
 }
