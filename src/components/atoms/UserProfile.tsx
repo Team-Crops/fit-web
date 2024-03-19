059cae45371b599fd938119c4d568f9242225345
Seheon Yu <seheon@seoultech.ac.kr>
2024-03-19 18:31:12 +0900

    Add UserProfile component

---
+++ src/components/atoms/UserProfile.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+import styled from '@emotion/styled';
+
+import { Icons } from './Icons';
+
+const ProfileImage = styled(Image)`
+  border-radius: 50%;
+`;
+
+interface UserProfileProps extends React.HTMLAttributes<HTMLDivElement> {
+  imageUrl?: string;
+  nickname?: string;
+  width?: number;
+  height?: number;
+}
+
+export function UserProfile({
+  imageUrl,
+  nickname,
+  width = 48,
+  height = 48,
+  ...props
+}: UserProfileProps) {
+  const alt = nickname ? `${nickname}'s profile image` : 'profile image';
+
+  if (imageUrl) {
+    return <ProfileImage src={imageUrl} alt={alt} width={width} height={height} {...props} />;
+  }
+
+  return <Icons icon="account" width={width} height={height} />;
+}
