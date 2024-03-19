636bf8dbe3c2cc949baa3a80b26ce597b1111a2b
Seheon Yu <seheon@seoultech.ac.kr>
2024-03-19 14:48:57 +0900

    Add TalkBubble component

--- src/components/molecules/TalkBubble.tsx
+++ src/components/molecules/TalkBubble.tsx
@@ -1,6 +1,27 @@
+import Image from 'next/image';
+
 import styled from '@emotion/styled';
 
+import { User } from '#/entities/user';
 import { Txt } from '#atoms/Text';
+import { Icons } from '../atoms/Icons';
+
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const BubbleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const ProfileImage = styled(Image)`
+  border-radius: 50%;
+`;
+
+const UserName = styled(Txt)``;
 
 const TextBubble = styled(Txt)<{ tailPosition: TalkBubbleProps['tailPosition'] }>`
   background: #fff;
@@ -13,16 +34,34 @@ const TextBubble = styled(Txt)<{ tailPosition: TalkBubbleProps['tailPosition'] }
 
 interface TalkBubbleProps extends React.HTMLAttributes<HTMLDivElement> {
   tailPosition?: 'left' | 'right';
+  user: User;
 }
 
-export function TalkBubble({ tailPosition = 'left', ...props }: TalkBubbleProps) {
+export function TalkBubble({ tailPosition = 'left', user, ...props }: TalkBubbleProps) {
   return (
-    <TextBubble
-      tailPosition={tailPosition}
-      size="typo4"
-      weight="regular"
-      color="#424242"
-      {...props}
-    />
+    <Container>
+      {user.profileImageUrl ? (
+        <ProfileImage
+          src={user.profileImageUrl}
+          alt={`${user.nickname}'s profile image`}
+          width={48}
+          height={48}
+        />
+      ) : (
+        <Icons icon="account" />
+      )}
+      <BubbleContainer>
+        <UserName size="typo5" weight="bold" color="#757575">
+          {user.nickname}
+        </UserName>
+        <TextBubble
+          tailPosition={tailPosition}
+          size="typo4"
+          weight="regular"
+          color="#424242"
+          {...props}
+        />
+      </BubbleContainer>
+    </Container>
   );
 }
