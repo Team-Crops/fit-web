e0e06932c3ab8165733515286f1fd8a2d3e20a61
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-03 18:14:53 +0900

    Refactor TalkBubble component to use reverse flex-direction in case

--- src/components/molecules/TalkBubble.tsx
+++ src/components/molecules/TalkBubble.tsx
@@ -4,8 +4,31 @@ import { User } from '#/types/user';
 import { Txt } from '#atoms/Text';
 import { UserProfile } from '#atoms/UserProfile';
 
-const Container = styled.div`
+interface TalkBubbleProps extends React.HTMLAttributes<HTMLDivElement> {
+  myBubble?: boolean;
+  user: User;
+}
+
+export const TalkBubble = ({ myBubble = false, user, ...props }: TalkBubbleProps) => {
+  return (
+    <Container myBubble={myBubble}>
+      <UserProfile imageUrl={user.profileImageUrl} nickname={user.nickname} />
+      <BubbleContainer myBubble={myBubble}>
+        <Txt size="typo5" weight="bold" color="#757575">
+          {user.nickname}
+        </Txt>
+        <TextBubble myBubble={myBubble} size="typo4" weight="regular" color="#424242" {...props} />
+      </BubbleContainer>
+      <BubbleTime size="typo6" weight="medium" color="#9E9E9E">
+        12:24 PM
+      </BubbleTime>
+    </Container>
+  );
+};
+
+const Container = styled.div<{ myBubble: boolean }>`
   display: flex;
+  flex-direction: ${({ myBubble }) => (myBubble ? 'row-reverse' : 'row')};
   gap: 10px;
 `;
 
@@ -31,33 +54,3 @@ const BubbleTime = styled(Txt)`
   margin-top: auto;
   white-space: nowrap;
 `;
-
-interface TalkBubbleProps extends React.HTMLAttributes<HTMLDivElement> {
-  myBubble?: boolean;
-  user: User;
-}
-
-export const TalkBubble = ({ myBubble = false, user, ...props }: TalkBubbleProps) => {
-  return (
-    <Container>
-      {myBubble && (
-        <BubbleTime size="typo6" weight="medium" color="#9E9E9E">
-          12:24 PM
-        </BubbleTime>
-      )}
-      {!myBubble && <UserProfile imageUrl={user.profileImageUrl} nickname={user.nickname} />}
-      <BubbleContainer myBubble={myBubble}>
-        <Txt size="typo5" weight="bold" color="#757575">
-          {user.nickname}
-        </Txt>
-        <TextBubble myBubble={myBubble} size="typo4" weight="regular" color="#424242" {...props} />
-      </BubbleContainer>
-      {myBubble && <UserProfile imageUrl={user.profileImageUrl} nickname={user.nickname} />}
-      {!myBubble && (
-        <BubbleTime size="typo6" weight="medium" color="#9E9E9E">
-          12:24 PM
-        </BubbleTime>
-      )}
-    </Container>
-  );
-};
