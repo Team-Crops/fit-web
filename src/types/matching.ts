373dfa2361863cc6a3ee875f8acd64ef8df82a00
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-03 20:49:29 +0900

    Add MatchingRoom and MatchingUser interfaces

--- src/types/matching.ts
+++ src/types/matching.ts
@@ -1,3 +1,5 @@
+import { User } from '#/types';
+
 export enum MatchingStatus {
   REGISTER = 'REGISTER',
   WAITING = 'WAITING',
@@ -11,10 +13,28 @@ export enum MatchingStatus {
 }
 
 export interface Matching {
+  roomId: number | null;
+  userId: number;
+  positionId: number;
+  status: MatchingStatus;
+
   expiredAt: string;
   createdAt: string;
-  positionId: number;
-  userId: number;
-  roomId: number | null;
-  status: MatchingStatus;
+}
+
+export interface MatchingRoom {
+  id: number;
+
+  hostId: User['id'];
+  chatId: number;
+  matchingUsers: MatchingUser[];
+  isCompleted: boolean;
+
+  completedAt: string | null;
+}
+
+export interface MatchingUser
+  extends Pick<User, 'id' | 'username' | 'profileImageUrl' | 'positionId'> {
+  isHost: boolean;
+  isReady: boolean;
 }
