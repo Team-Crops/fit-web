8cb68e5c7fed2e94ba8fe8c4712414bdf8740039
jaewoongs <dbswodnd5428@gmail.com>
2024-05-16 16:26:45 +0900

    fix, 파일 이름에 맞게 수정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -35,5 +35,5 @@ export { default as PhoneFillIcon } from './phone-fill.svg';
 export { default as FacebookIcon } from './facebook.svg';
 export { default as GithubIcon } from './github.svg';
 export { default as VelogIcon } from './velog.svg';
-export { default as LinkedInIcon } from './linkedin.svg';
+export { default as LinkedInIcon } from './linkedIn.svg';
 export { default as TistoryIcon } from './tistory.svg';
