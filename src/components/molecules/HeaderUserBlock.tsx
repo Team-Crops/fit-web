579057bcae357a7cd17d01cdf3b98f62821af860
jaewoongs <dbswodnd5428@gmail.com>
2024-05-23 02:34:13 +0900

    feat, header 프로필 눌렀을 때 나오는 모달 추가

--- src/components/molecules/HeaderUserBlock.tsx
+++ src/components/molecules/HeaderUserBlock.tsx
@@ -1,6 +1,9 @@
+import { useCallback, useState } from 'react';
+
 import styled from '@emotion/styled';
 
 import { Icons } from '#atoms/Icons';
+import { HeaderMenuModal } from './HeaderMenuModal';
 import { ProfileBlock } from '../organisms/ProfileBlock';
 
 const FlexBlock = styled.div`
@@ -8,12 +11,35 @@ const FlexBlock = styled.div`
   gap: 36px;
   align-items: center;
 `;
+const MenuBlock = styled.div`
+  cursor: pointer;
+  position: relative;
+  z-index: 2;
+`;
+const Background = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 0;
 
+  width: 100vw;
+  height: 100vh;
+`;
 export const HeaderUserBlock = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  const menuToggle = useCallback(() => {
+    setIsOpenMenu(!isOpenMenu);
+  }, [isOpenMenu]);
+
   return (
     <FlexBlock>
       <Icons icon={'bell'} width={28} height={35} />
-      <ProfileBlock size={45} />
+      <MenuBlock onClick={menuToggle}>
+        <ProfileBlock size={45} />
+        <HeaderMenuModal isOpen={isOpenMenu} menuToggle={menuToggle} />
+      </MenuBlock>
+      {isOpenMenu && <Background onClick={menuToggle} />}
     </FlexBlock>
   );
 };
