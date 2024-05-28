52b832fd3b496ea68c1e7790b983e2109cd39878
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-29 00:55:55 +0900

    fix: Only one policy can be expanded

--- src/components/organisms/SignUpTermsPopup.tsx
+++ src/components/organisms/SignUpTermsPopup.tsx
@@ -22,6 +22,7 @@ export const SignUpTermsPopup: React.FC<SignUpTermsPopupProps> = ({ onSuccess })
       {} as Record<PolicyType, PolicyAgreement>
     )
   );
+  const [expandedPolicy, setExpandedPolicy] = useState<PolicyType | null>(null);
 
   const isAllAgreed = useMemo(() => {
     return Object.values(policyAgrees).every(({ isAgreed }) => isAgreed);
@@ -64,7 +65,9 @@ export const SignUpTermsPopup: React.FC<SignUpTermsPopupProps> = ({ onSuccess })
             title={policy.title}
             type={policy.type}
             disabled={isLoadingAgrees}
+            expanded={expandedPolicy === policy.type}
             value={policyAgrees[policy.type].isAgreed}
+            onClick={() => setExpandedPolicy((t) => (policy.type === t ? null : policy.type))}
             onChange={(e) =>
               setPolicyAgrees(
                 produce((draft) => {
@@ -99,6 +102,7 @@ const Container = styled.div`
   justify-content: space-between;
 
   width: min(100%, 830px);
+  max-height: 100vh;
 
   background-color: #fff;
   border-radius: 15px;
