4e1e8f057cfdae7d848c4432e98ef70286b3e733
Seheon Yu <seheon@seoultech.ac.kr>
2024-05-29 18:15:22 +0900

    feature: Add notice message type

--- src/types/message.ts
+++ src/types/message.ts
@@ -2,19 +2,31 @@ import { User } from '.';
 
 export interface Message {
   id: number;
-  userId: User['id'];
-  messageType: 'TEXT' | 'IMAGE';
+  messageType: 'TEXT' | 'IMAGE' | 'NOTICE';
   createdAt: string;
+  userId?: User['id'];
   content?: string;
   imageUrl?: string;
+  notice?: string;
 }
 
 export interface TextMessage extends Message {
   messageType: 'TEXT';
   content: string;
+  imageUrl: undefined;
+  notice: undefined;
 }
 
 export interface ImageMessage extends Message {
   messageType: 'IMAGE';
+  content: undefined;
   imageUrl: string;
+  notice: undefined;
+}
+
+export interface NoticeMessage extends Message {
+  messageType: 'NOTICE';
+  content: undefined;
+  imageUrl: undefined;
+  notice: string;
 }
