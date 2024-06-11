44f9b3fd801843f72de3766e8b73a8635a883679
Seheon Yu <seheon@seoultech.ac.kr>
2024-06-11 11:24:03 +0900

    refactor: Update socket URL to use environment variable

--- src/utilities/socket.ts
+++ src/utilities/socket.ts
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import { getTokens } from '#/utilities/session';
 
 export const fitSocket = (query: Record<string, string | number | null | undefined>) =>
-  io('ws://dev-socket.f-it.team', {
+  io(process.env.NEXT_PUBLIC_SOCKET_URL, {
     path: '',
     query: { ...query, auth: getTokens()?.accessToken },
     transports: ['websocket'],
