3b781df09d9df98c07f0d2dccde5205d3bba35b3
Seheon Yu <seheon@seoultech.ac.kr>
2024-06-14 00:24:39 +0900

    refactor: Change ApiError interface to class

--- src/types/api-error.ts
+++ src/types/api-error.ts
@@ -1,4 +1,21 @@
-export interface ApiError extends Error {
+export class ApiError {
+  static readonly INVALID_ACCESS_TOKEN_CODE = 'auth-4';
+
+  static readonly MATCHING_ALREADY_STARTED_CODE = 'matching-1';
+  static readonly MATCHING_NOT_FOUND_CODE = 'matching-2';
+
   code: string;
   message: string;
+
+  constructor(code: string, message: string) {
+    this.code = code;
+    this.message = message;
+  }
+
+  static isApiError(error: any): error is ApiError {
+    if (!error) {
+      return false;
+    }
+    return typeof error.code === 'string' && typeof error.message === 'string';
+  }
 }
