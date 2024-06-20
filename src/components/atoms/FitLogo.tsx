3c4d8ae046d81845963772ec4d0b2a8b3423d070
Seheon Yu <seheon@seoultech.ac.kr>
2024-06-20 14:38:42 +0900

    feature: Add symbol logo

--- src/components/atoms/FitLogo.tsx
+++ src/components/atoms/FitLogo.tsx
@@ -1,18 +1,29 @@
-import { SignatureLogo, MonochromeSignatureLogo } from '#/assets/logos';
+import { SignatureLogo, MonochromeSignatureLogo, SymbolLogo } from '#/assets/logos';
 
 interface LogoProps {
+  variant?: 'signature' | 'symbol';
   height?: number;
   color?: 'primary' | 'gray' | string;
 }
-export const FitLogo: React.FC<LogoProps> = ({ height = 40, color = 'primary' }) => {
-  const aspectRatio = 120 / 40;
-  return color === 'primary' ? (
-    <SignatureLogo width={height * aspectRatio} height={height} />
-  ) : (
-    <MonochromeSignatureLogo
-      width={height * aspectRatio}
-      height={height}
-      color={color === 'gray' ? '#757575' : color}
-    />
-  );
+export const FitLogo: React.FC<LogoProps> = ({
+  variant = 'signature',
+  height = 40,
+  color = 'primary',
+}) => {
+  const colors: Record<string, string> = {
+    primary: '#FF706C',
+    gray: '#757575',
+  };
+  const pickedColor = colors[color] ?? color;
+
+  if (variant === 'signature') {
+    const aspectRatio = 120 / 40;
+    return color === 'primary' ? (
+      <SignatureLogo width={height * aspectRatio} height={height} />
+    ) : (
+      <MonochromeSignatureLogo width={height * aspectRatio} height={height} color={pickedColor} />
+    );
+  } else if (variant === 'symbol') {
+    return <SymbolLogo width={height} height={height} color={pickedColor} />;
+  }
 };
