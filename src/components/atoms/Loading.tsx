2af427f4522056a7ded3f8e4e8ebc83fd5fb90c2
Seheon Yu <seheon@seoultech.ac.kr>
2024-06-22 21:12:48 +0900

    refactor: Add spin-elastic-out animation to Loading component

--- src/components/atoms/Loading.tsx
+++ src/components/atoms/Loading.tsx
@@ -1,7 +1,20 @@
 import { forwardRef } from 'react';
 
+import styled from '@emotion/styled';
+
+import { FitLogo } from '.';
+
 export const Loading = forwardRef<HTMLDivElement>(({}, ref) => {
-  return <div ref={ref}>Loading...</div>;
+  return (
+    <Container ref={ref}>
+      <FitLogo variant="symbol" />
+    </Container>
+  );
 });
 
 Loading.displayName = 'Loading';
+
+const Container = styled.div`
+  display: inline-block;
+  animation: spin-elastic-out 2s infinite;
+`;
