1ad14f4e208ff989b405529e61c39fddb6ddefc8
Seheon Yu <seheon@seoultech.ac.kr>
2024-06-24 20:21:03 +0900

    chore: Update Loading component to use Spinner for loading animation

--- src/components/atoms/Loading.tsx
+++ src/components/atoms/Loading.tsx
@@ -7,21 +7,21 @@ import { FitLogo } from '.';
 export const Loading = forwardRef<HTMLDivElement>(({}, ref) => {
   return (
     <Container ref={ref}>
-      <FlexBlock>
+      <Spinner>
         <FitLogo variant="symbol" />
-      </FlexBlock>
+      </Spinner>
     </Container>
   );
 });
 
 Loading.displayName = 'Loading';
 
-const FlexBlock = styled.div`
-  display: flex;
-  width: fit-content;
-  animation: spin-elastic-out 2s infinite;
-`;
 const Container = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+const Spinner = styled.div`
+  width: fit-content;
+  animation: spin-elastic-out 2s infinite;
 `;
