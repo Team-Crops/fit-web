6a827efa3f14d589867a969a99664b52214a791d
Seheon Yu <seheon@seoultech.ac.kr>
2024-06-27 23:19:55 +0900

    feature: Get ids query params to use as unfoldedProjectIds

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,19 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 import { LoginGuard } from '#/app/login-guard';
 import { Projects } from '#/components/templates/Projects';
 
 export default function ProjectsPage() {
+  const searchParams = useSearchParams();
+
+  const idsParam = searchParams.get('ids');
+  const projectIds = idsParam?.split('.').map((id) => parseInt(id));
+
   return (
     <LoginGuard>
-      <Projects />
+      <Projects unfoldedProjectIds={projectIds} />
     </LoginGuard>
   );
 }
