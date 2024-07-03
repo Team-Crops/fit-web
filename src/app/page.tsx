e28c57414c7916b50094dcfa573c5fd52935713c
Seheon Yu <seheon@seoultech.ac.kr>
2024-07-03 23:11:01 +0900

    refactor: Remove unused image files and update imports in main section 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { FirstSection } from '#templates/main/FirstSection';
+import { MainSection1 } from '#/components/templates/MainSection1';
 import { FourthSection } from '#templates/main/FourthSection';
 import { SecondSection } from '#templates/main/SecondSection';
 import { ThirdSection } from '#templates/main/ThirdSection';
@@ -6,7 +6,7 @@ import { ThirdSection } from '#templates/main/ThirdSection';
 export default function Home() {
   return (
     <>
-      <FirstSection />
+      <MainSection1 />
       <SecondSection />
       <ThirdSection />
       <FourthSection />
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,13 +1,32 @@
-export { default as positionDesignerPic } from './position-designer.png';
-export { default as positionMobileDeveloperPic } from './position-mobile-developer.png';
-export { default as positionPlannerPic } from './position-planner.png';
-export { default as positionServerDeveloperPic } from './position-server-developer.png';
-export { default as positionWebDeveloperPic } from './position-web-developer.png';
-
-export { default as positionPageBackground1 } from './position-page-background-1.png';
-export { default as positionPageBackground2 } from './position-page-background-2.png';
+export { default as mainMatching1 } from './main-matching-1.svg';
+export { default as mainMatching2 } from './main-matching-2.svg';
+export { default as mainMatching3 } from './main-matching-3.svg';
+export { default as mainMatchingBackground } from './main-matching-background.png';
+export { default as mainProject1 } from './main-project-1.svg';
+export { default as mainProject2 } from './main-project-2.svg';
+export { default as mainProjectBackground } from './main-project-background.png';
+export { default as mainRecommend1 } from './main-recommend-1.svg';
+export { default as mainRecommend2 } from './main-recommend-2.svg';
+export { default as mainRecommendBackground } from './main-recommend-background.png';
+export { default as mainSection1Background } from './main-section-1-background.png';
+export { default as mainSection1Character } from './main-section-1-character.png';
+export { default as mainSection1Fragment1 } from './main-section-1-fragment-1.png';
+export { default as mainSection1Fragment2 } from './main-section-1-fragment-2.png';
+export { default as mainSection1Fragment3 } from './main-section-1-fragment-3.png';
+export { default as mainSection1Logo } from './main-section-1-logo.png';
+export { default as mainSection2 } from './main-section-2.svg?url';
+export { default as mainSection3Arrow } from './main-section-3-arrow.svg';
+export { default as mainSection3Background } from './main-section-3-background.svg';
 
 export { default as matchingPageBackground1 } from './matching-page-background-1.png';
 export { default as matchingPageDoughnut } from './matching-page-doughnut.png';
 
 export { default as notFound } from './not-found.png';
+
+export { default as positionDesignerPic } from './position-designer.png';
+export { default as positionMobileDeveloperPic } from './position-mobile-developer.png';
+export { default as positionPageBackground1 } from './position-page-background-1.png';
+export { default as positionPageBackground2 } from './position-page-background-2.png';
+export { default as positionPlannerPic } from './position-planner.png';
+export { default as positionServerDeveloperPic } from './position-server-developer.png';
+export { default as positionWebDeveloperPic } from './position-web-developer.png';
