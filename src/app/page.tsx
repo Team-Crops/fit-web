e72a72f8376dd57b8757a00cefadbe821a2c8974
Seheon Yu <seheon@seoultech.ac.kr>
2024-07-04 00:23:46 +0900

    refactor: Update imports and remove unused image files in main section 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { MainSection1 } from '#/components/templates/MainSection1';
 import { MainSection2 } from '#/components/templates/MainSection2';
+import { MainSection3 } from '#/components/templates/MainSection3';
 
 export default function Home() {
   return (
     <>
       <MainSection1 />
       <MainSection2 />
-      {/* <ThirdSection /> */}
+      <MainSection3 />
       {/* <FourthSection /> */}
     </>
   );
