c66ee9c76e20b024f3f21d387bf70c56b9f6ca02
Seheon Yu <seheon@seoultech.ac.kr>
2024-07-04 16:03:26 +0900

    refactor: Update imports and remove unused image files in recommend intro block of main section 4

--- src/components/organisms/main/RecommendBlock.tsx
+++ src/components/organisms/main/RecommendBlock.tsx
@@ -2,13 +2,25 @@ import Image from 'next/image';
 
 import styled from '@emotion/styled';
 
+import {
+  mainSection3Recommend1,
+  mainSection3Recommend2,
+  mainSection3RecommendBackground,
+} from '#/assets/images';
 import { MainDescriptionBlock } from '#molecules/MainDescriptionBlock';
 import { MainDescriptionCard } from '#molecules/MainDescriptionCard';
 
 const Block = styled.div`
   position: relative;
-  width: 1194px;
-  margin-bottom: 389px;
+
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+
+  width: 100vw;
+  max-width: 1920px;
+  padding: 20px;
 `;
 const CardWrapper = styled.div`
   display: flex;
@@ -23,20 +35,14 @@ const BackgroundImage = styled(Image)`
 
 const CardInfo = [
   {
-    width: 592,
     title: '팀원 특징 설정',
     description: `구하려는 팀원의 포지션, 프로젝트 경험,\n보유한 기술 등을 선택해주세요!`,
-    imgUrl: '/images/main_recommend_img1.svg',
-    imgWidth: 361,
-    imgHeight: 245,
+    image: mainSection3Recommend1,
   },
   {
-    width: 583,
     title: '팀원 추천',
     description: `설정한 조건에 맞는\n예비 팀원들을 추천해줄게요!`,
-    imgUrl: '/images/main_recommend_img2.svg',
-    imgWidth: 358,
-    imgHeight: 262,
+    image: mainSection3Recommend2,
   },
 ];
 
@@ -44,35 +50,26 @@ export const RecommendBlock = () => {
   return (
     <Block>
       <MainDescriptionBlock
-        title={'랜덤 팀 매칭'}
-        bigDescription={`클릭 한 번으로\n나와 꼭 맞는 팀과 매칭`}
-        smallDescription={'기획-디자인-개발, 최적의 팀원들을 단 3일 안에 매칭!'}
-        buttonText={'매칭하러 가기'}
-        buttonLink={'/matchingInfo'}
+        title={'팀원 추천'}
+        bigDescription={`내가 원하는 팀원과 함께 할 수 있는 기회`}
+        smallDescription={'원하는 팀원의 특징을 설정해 추천을 받아보세요.'}
+        buttonText={'추천 받기'}
+        buttonLink={'/team-recommend'}
       />
       <CardWrapper>
-        {CardInfo.map((card, index) => {
-          return (
-            <MainDescriptionCard
-              key={index}
-              width={card.width}
-              height={500}
-              index={index + 1}
-              title={card.title}
-              description={card.description}
-              imgUrl={card.imgUrl}
-              imgWidth={card.imgWidth}
-              imgHeight={card.imgHeight}
-            />
-          );
-        })}
+        {CardInfo.map((card, index) => (
+          <MainDescriptionCard
+            key={index}
+            width={600}
+            height={500}
+            index={index + 1}
+            title={card.title}
+            description={card.description}
+            image={card.image}
+          />
+        ))}
       </CardWrapper>
-      <BackgroundImage
-        src={'/images/main_recommend_background.png'}
-        width={102}
-        height={102}
-        alt={'background'}
-      />
+      <BackgroundImage src={mainSection3RecommendBackground} alt={'background'} />
     </Block>
   );
 };
